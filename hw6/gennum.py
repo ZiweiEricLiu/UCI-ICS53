07a6554adb3c026e15a8ca43489fcdba18655fec
LittlMeng <ziweiericliu@gmail.com>
2021-09-19 14:28:22 -0700

    Delete hw6 directory

--- hw6/gennum.py
+++
@@ -1,9 +0,0 @@
-# usage: python gennum.py NUMBER_OF_DIGITS
-# use > redirection to create files
-import random
-import sys
-
-i = int(sys.argv[1])
-while i > 0:
-    sys.stdout.write(str(random.randint(0,9)))
-    i -= 1
--- hw6/include/stats.h
+++
@@ -1,8 +0,0 @@
-#ifndef STATS_H
-#define STATS_H
-
-float calc_mean_median(int sum, int n, int *cfreq, float *median);
-int histdata(int *copy);
-int statdata(float *mean, float *median, int *mode);
-
-#endif

3b1cf69db43df02a64ec965c589354da7cb467c5
LittlMeng <ziweiericliu@gmail.com>
2021-09-19 14:20:19 -0700

    Delete older version folder

--- hw4/include/helpers.h
+++
@@ -1,7 +0,0 @@
-// A header file for helpers.c
-// Declare any additional functions in this file
-#include "shell_util.h"
-#include "linkedList.h"
-
-int timeComparator(void* pe1, void* pe2);
-ProcessEntry_t* findPeFromList(List_t* l, int pid); 
